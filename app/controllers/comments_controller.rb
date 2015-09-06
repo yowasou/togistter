cba5112654a2e1ae6a0e36e89fe1d1a63dafc843
yowasou <jun.makita@gmail.com>
2015-09-06 16:15:10 +0900

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,9 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     @comment = Comment.new
+    gid = params[:gist_id]
+    @comment.gist_id = gid
+    @gist = Gist.find(gid)
   end
 
   # GET /comments/1/edit
@@ -25,11 +28,14 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-
+    gid = params[:gist_id]
+    @gist = Gist.find(gid)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        #format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        #format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to @gist.matome, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @gist.matome }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
